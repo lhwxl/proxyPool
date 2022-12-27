737b9b9fce8693480b2025a9ade8868c3b21cb05
lhwxl <lhw100501@163.com>
2022-12-27 16:12:26 +0800

    添加代理列表

---
+++ run.py
@@ -0,0 +1,8 @@
+ip_proxy_webs = ["http://www.66ip.cn/index.html",
+                 "https://proxy.seofangfa.com/",
+                 "https://www.xsdaili.cn/dayProxy/ip/1872.html",
+                 "https://www.89ip.cn/index.html",
+                 "https://www.kuaidaili.com/free/inha/",
+                 "http://ip.yqie.com/ipproxy.htm",
+                 "https://ip.jiangxianli.com/",
+                 "http://www.ip3366.net/free/"]
